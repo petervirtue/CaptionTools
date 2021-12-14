72f34a85f645b585d58ae20600b470290751ad05
Peter Virtue <petervirtue@MacBook-Pro.local>
2021-12-13 21:20:10 -0500

    Home cleanup

--- CaptionTools/Controllers/Home/HomeController.swift
+++ CaptionTools/Controllers/Home/HomeController.swift
@@ -17,11 +17,9 @@ class HomeController: UIViewController, UITextViewDelegate {
     let italicChars = ["0":"0","1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8","9":"9","a":"𝘢","b":"𝘣","c":"𝘤","d":"𝘥","e":"𝘦","f":"𝘧","g":"𝘨","h":"𝘩","i":"𝘪","j":"𝘫","k":"𝘬","l":"𝘭","m":"𝘮","n":"𝘯","o":"𝘰","p":"𝘱","q":"𝘲","r":"𝘳","s":"𝘴","t":"𝘵","u":"𝘶","v":"𝘷","w":"𝘸","x":"𝘹","y":"𝘺","z":"𝘻","A":"𝘈","B":"𝘉","C":"𝘊","D":"𝘋","E":"𝘌","F":"𝘍","G":"𝘎","H":"𝘏","I":"𝘐","J":"𝘑","K":"𝘒","L":"𝘓","M":"𝘔","N":"𝘕","O":"𝘖","P":"𝘗","Q":"𝘘","R":"𝘙","S":"𝘚","T":"𝘛","U":"𝘜","V":"𝘝","W":"𝘞","X":"𝘟","Y":"𝘠","Z":"𝘡"]
     
     // Shared instance
-    
     static let sharedInstance = HomeController()
     
     // Components
-    
     var captionIn: UITextView!
     var previewButton: UIButton!
     var shareButton: UIButton!
@@ -33,27 +31,22 @@ class HomeController: UIViewController, UITextViewDelegate {
     var hashtagsImage: UIImageView!
     
     // Bottom constraint for Text Field
-    
     var captionInBottom: NSLayoutConstraint!
 
     // Editing status
-    
     var typingStatus = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Controller setup
-        
         self.title = "Create"
         self.edgesForExtendedLayout = []
         
         // Run Setup
-        
         setup()
         
         // Text View Delegate
-        
         captionIn.delegate = self
         
     }
